d1840f3ff465c10f2d514019fb310813d12d789f
Bokonon79 <schoenbeck@gmail.com>
2019-01-03 02:30:47 -0500

    Adding the rocket back

--- Dollop/Program.cs
+++ Dollop/Program.cs
@@ -7,6 +7,7 @@ namespace Dollop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Mars!");
+            Console.WriteLine("---===|000>");
         }
     }
 }
