93d61bb0f35716c868d5fbe35183945a3b7bff36
Bokonon79 <schoenbeck@gmail.com>
2019-01-03 02:34:23 -0500

    Made the rocket bigger

--- Dollop/Program.cs
+++ Dollop/Program.cs
@@ -7,7 +7,7 @@ namespace Dollop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Mars!");
-            Console.WriteLine("---===|000>");
+            Console.WriteLine("---===|00000000>");
         }
     }
 }
